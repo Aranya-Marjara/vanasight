7693bb7a0338ac010108bc3c236260b4b2cd952c
5hridhyan <5hridhyanx@gmail.com>
2025-11-01 15:12:40 +0530

    Update __init__.py

--- vanasight/__init__.py
+++ vanasight/__init__.py
@@ -5,6 +5,6 @@ VanaSight: Forest Vision - From Pixels to Perception
 __version__ = "1.0.0"
 __author__ = "Aranya-Marjara"
 
-from .pipeline import VanaSight  # Make sure this matches the class name
+from .pipeline import VanaSight  
 
 __all__ = ['VanaSight']
